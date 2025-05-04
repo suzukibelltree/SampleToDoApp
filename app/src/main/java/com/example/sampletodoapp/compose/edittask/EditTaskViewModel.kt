ac73cf99d989be9ef8986c9440c2156b0cf10b1e
suzukibelltree <suzuki879455@gmail.com>
2025-05-04 15:08:05 +0900

    タスクの詳細を編集できる機能を実装

--- app/src/main/java/com/example/sampletodoapp/compose/edittask/EditTaskViewModel.kt
+++ app/src/main/java/com/example/sampletodoapp/compose/edittask/EditTaskViewModel.kt
@@ -4,13 +4,27 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.sampletodoapp.room.Task
+import com.example.sampletodoapp.room.TaskPriority
 import com.example.sampletodoapp.room.TasksRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+
+sealed interface EditTaskUiState {
+    data class Edit(
+        val id: Int,
+        val title: String,
+        val deadline: String,
+        val importance: TaskPriority,
+        val isDone: Boolean
+    ) : EditTaskUiState
+
+    data object Loading : EditTaskUiState
+    data object Success : EditTaskUiState
+}
 
 @HiltViewModel
 class EditTaskViewModel @Inject constructor(
@@ -19,17 +33,69 @@ class EditTaskViewModel @Inject constructor(
 ) : ViewModel() {
     private val taskId = checkNotNull(savedStateHandle.get<Int>("taskId"))
 
-    val taskFlow: StateFlow<Task?> = tasksRepository.loadTaskById(taskId)
-        .stateIn(
-            viewModelScope,
-            SharingStarted.WhileSubscribed(5000),
-            null
-        )
+    private val _uiState = MutableStateFlow<EditTaskUiState>(EditTaskUiState.Loading)
+    val uiState: StateFlow<EditTaskUiState> = _uiState
+
+    init {
+        viewModelScope.launch {
+            val task = tasksRepository.loadTaskById(taskId).first()
+            _uiState.value = EditTaskUiState.Edit(
+                id = task.id,
+                title = task.title,
+                deadline = task.deadline,
+                importance = TaskPriority.fromLevel(task.importance),
+                isDone = task.isDone
+            )
+        }
+    }
+
+    // タスクのタイトルの更新
+    fun updateTitle(newTitle: String) {
+        val current = _uiState.value
+        if (current is EditTaskUiState.Edit) {
+            _uiState.value = current.copy(title = newTitle)
+        }
+    }
+
+    // タスクの期限日の更新
+    fun updateDeadline(newDeadline: String) {
+        val current = _uiState.value
+        if (current is EditTaskUiState.Edit) {
+            _uiState.value = current.copy(deadline = newDeadline)
+        }
+    }
+
+    // タスクの重要度の更新
+    fun updateImportance(newImportance: Int) {
+        val current = _uiState.value
+        if (current is EditTaskUiState.Edit) {
+            _uiState.value = current.copy(importance = TaskPriority.fromLevel(newImportance))
+        }
+    }
+
+    // タスクの完了状態の更新
+    fun toggleIsDone() {
+        val current = _uiState.value
+        if (current is EditTaskUiState.Edit) {
+            _uiState.value = current.copy(isDone = !current.isDone)
+        }
+    }
 
     // タスクの更新
-    fun updateTask(task: Task) {
-        viewModelScope.launch {
-            tasksRepository.updateTask(task)
+    fun saveTask() {
+        val current = _uiState.value
+        if (current is EditTaskUiState.Edit) {
+            viewModelScope.launch {
+                val updatedTask = Task(
+                    id = current.id,
+                    title = current.title,
+                    deadline = current.deadline,
+                    importance = current.importance.level,
+                    isDone = current.isDone
+                )
+                tasksRepository.updateTask(updatedTask)
+                _uiState.value = EditTaskUiState.Success
+            }
         }
     }
 }
--- app/src/main/java/com/example/sampletodoapp/compose/edittask/EdittaskScreen.kt
+++ app/src/main/java/com/example/sampletodoapp/compose/edittask/EdittaskScreen.kt
@@ -1,20 +1,132 @@
 package com.example.sampletodoapp.compose.edittask
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.sampletodoapp.compose.addtask.DatePickerModal
+import com.example.sampletodoapp.compose.addtask.ImportanceRadioButtons
+import com.example.sampletodoapp.compose.addtask.convertMillisToDate
+import kotlinx.coroutines.launch
 
 @Composable
 fun EditTaskScreen(
     viewModel: EditTaskViewModel = hiltViewModel(),
     navController: NavController,
 ) {
-    val task = viewModel.taskFlow.collectAsState()
-    task.value?.let {
-        Text(
-            text = it.title
-        )
+    val uiState = viewModel.uiState.collectAsState()
+    when (uiState.value) {
+        is EditTaskUiState.Loading -> {
+            Text(text = "Loading...")
+        }
+
+        is EditTaskUiState.Edit -> {
+            val task = uiState.value as EditTaskUiState.Edit
+            val scope = rememberCoroutineScope()
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                var showDatePicker by remember { mutableStateOf(false) }
+                // タスク名の入力
+                OutlinedTextField(
+                    value = task.title,
+                    onValueChange = { viewModel.updateTitle(newTitle = it) },
+                    label = { Text("タスクを入力") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    singleLine = true
+                )
+                // 期限の選択
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(text = "期限日: ${task.deadline}", fontSize = 20.sp)
+                    OutlinedButton(
+                        onClick = { showDatePicker = true }
+                    ) {
+                        Text(
+                            text = "期日の選択",
+                        )
+                    }
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(text = "重要度")
+                    ImportanceRadioButtons(
+                        selected = task.importance,
+                        onSelected = { viewModel.updateImportance(newImportance = it.level) }
+                    )
+                }
+                // トグルボタンでタスクの完了状態を変更
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "タスクの状態：${if (task.isDone) "完了" else "未完了"}",
+                    )
+                    Switch(
+                        checked = task.isDone,
+                        onCheckedChange = {
+                            viewModel.toggleIsDone()
+                        },
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+                Button(
+                    onClick = {
+                        scope.launch {
+                            viewModel.saveTask() // タスクを保存
+                        }
+                        navController.navigate("home")
+                    }
+                ) {
+                    Text(text = "更新")
+                }
+                if (showDatePicker) {
+                    DatePickerModal(
+                        onDateSelected = { date ->
+                            showDatePicker = false
+                            viewModel.updateDeadline(
+                                newDeadline = date?.let { convertMillisToDate(it) } ?: ""
+                            )
+                        },
+                        onDismiss = { showDatePicker = false }
+                    )
+                }
+            }
+        }
+
+        is EditTaskUiState.Success -> {
+            Text(text = "Success")
+        }
     }
 }
