add9ceb0ccb0235e0a868a3e5d8c5e9dd1df8bdb
suzukibelltree <suzuki879455@gmail.com>
2025-05-14 17:16:19 +0900

    Taskクラスに進捗度のプロパティを追加し、マイグレーション処理を記述

--- app/src/main/java/com/example/sampletodoapp/room/Task.kt
+++ app/src/main/java/com/example/sampletodoapp/room/Task.kt
@@ -3,6 +3,15 @@ package com.example.sampletodoapp.room
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+/**
+ * タスクのデータクラス
+ * @param id タスクのID
+ * @param title タスクのタイトル
+ * @param deadline タスクの期限日
+ * @param importance タスクの重要度
+ * @param progress タスクの進捗度
+ * @param isDone タスクの完了状態
+ */
 @Entity(tableName = "tasks")
 data class Task(
     @PrimaryKey(autoGenerate = true)
@@ -10,5 +19,6 @@ data class Task(
     val title: String,
     val deadline: String,
     val importance: Int,
+    val progress: Int = 0,
     val isDone: Boolean = false
 )
--- app/src/main/java/com/example/sampletodoapp/room/TaskDatabase.kt
+++ app/src/main/java/com/example/sampletodoapp/room/TaskDatabase.kt
@@ -3,8 +3,10 @@ package com.example.sampletodoapp.room
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database(entities = [Task::class], version = 1)
+@Database(entities = [Task::class], version = 2)
 abstract class TaskDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
 
@@ -12,11 +14,19 @@ abstract class TaskDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: TaskDatabase? = null
 
+        private val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                db.execSQL("ALTER TABLE tasks ADD COLUMN progress INTEGER NOT NULL DEFAULT 0")
+            }
+        }
+
+
         fun getDatabase(context: android.content.Context): TaskDatabase {
             return INSTANCE ?: synchronized(this) {
                 Room.databaseBuilder(
                     context, TaskDatabase::class.java, "task_database"
                 )
+                    .addMigrations(MIGRATION_1_2)
                     .build()
                     .also { INSTANCE = it }
             }
