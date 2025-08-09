e718eafbc9c49386a22c5deb93951a0b22c77502
suzukibelltree <suzuki879455@gmail.com>
2025-08-09 10:59:59 +0900

    AddTaskScreenのUIテストを作成

--- app/src/main/java/com/example/sampletodoapp/TodoAppNavigation.kt
+++ app/src/main/java/com/example/sampletodoapp/TodoAppNavigation.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -58,6 +59,7 @@ fun TodoAppNavigation(
             if (showFab) {
                 FloatingActionButton(
                     onClick = { navController.navigate(Route.AddTask) },
+                    modifier = Modifier.testTag("fab_add_task")
                 ) {
                     Icon(Icons.Default.Add, contentDescription = "Add Task")
                 }
